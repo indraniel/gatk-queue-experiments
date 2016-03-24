29fbf0d6e7e2d586fda18f8d536b71f34bd37f82
Indraniel Das <idas@genome.wustl.edu>
2016-03-24 14:37:03 -0500

    + initial commit

---
+++ Hello.q
@@ -0,0 +1,19 @@
+package org.broadinstitute.sv.HelloWorld
+
+import org.broadinstitute.gatk.queue.QScript
+
+import org.broadinstitute.gatk.utils.jna.lsf.v7_0_6.LibBat
+import org.broadinstitute.gatk.utils.jna.lsf.v7_0_6.LibBat.submit
+import org.broadinstitute.gatk.utils.jna.lsf.v7_0_6.LibLsf
+
+class HelloWorld extends QScript {
+  def script = {
+    add(new CommandLineFunction {
+      def commandLine = "echo hello world"
+    })
+
+    add(new CommandLineFunction {
+      def commandLine = "echo hello world 2"
+    })
+  }
+}
---
+++ test.sh
@@ -0,0 +1,26 @@
+
+export SV_DIR=/gscmnt/gc2801/analytics/jeldred/GenomeSTRiP/svtoolkit
+HELLO=/gscmnt/gc2801/analytics/jeldred/scripts/Hello.q
+
+JAVA_HOME=/gapp/x64linux/opt/java/jdk/jdk1.8.0_60
+export PATH=$JAVA_HOME/bin:/gscmnt/gc2801/analytics/jeldred/GenomeSTRiP/bin:$PATH
+
+# linus261 specific
+export LD_LIBRARY_PATH=/tmp/idas-local2/lib:$LD_LIBRARY_PATH
+
+echo $PATH
+#export PATH=/gscmnt/gc2801/analytics/jeldred/drmaa_versions/lsf_drmaa-1.0.4:/gscmnt/gc2801/analytics/jeldred/GenomeSTRiP/bin:$PATH
+IDAS_GATK=/gscmnt/gc2802/halllab/idas/jira/BIO-1662/vendor/local/jars/GenomeAnalysisTK-3.5-idas-experimental-e4c9106-2016.03.23.jar
+
+#classpath="${SV_DIR}/lib/SVToolkit.jar:${SV_DIR}/lib/gatk/GenomeAnalysisTK.jar:${SV_DIR}/lib/gatk/Queue.jar"
+classpath="${SV_DIR}/lib/SVToolkit.jar:${IDAS_GATK}:${SV_DIR}/lib/gatk/Queue.jar"
+
+java -version
+#java -Xmx22g -cp ${classpath} org.broadinstitute.gatk.queue.QCommandLine \
+java -cp ${classpath} org.broadinstitute.gatk.queue.QCommandLine \
+ -S ${HELLO} \
+ -disableJobReport \
+ -l DEBUG \
+ -jobRunner Drmaa \
+ -jobQueue short \
+ -run
