f0bbbb247ba1f9a66eecf08616447094b86d2791
Indraniel Das <idas@genome.wustl.edu>
2016-03-31 15:54:54 -0500

    + why must i init the session twice????

--- drmaa-gatk-scala/MultipleJobs.scala
+++ drmaa-gatk-scala/MultipleJobs.scala
@@ -3,46 +3,72 @@ import org.broadinstitute.gatk.utils.jna.drmaa.v1_0.JnaSessionFactory
 import org.broadinstitute.gatk.queue.QException
 import org.ggf.drmaa._
 
-object MultipleJobs {
-  def main(args: Array[String]): Unit = {
-    for (i <- 1 to 2) {
-      jobExecutor(i)
+class MultipleJobs {
+  var session: org.ggf.drmaa.Session = _
+  var setup: Int = 0
+
+  def init(): Unit = {
+    session = new JnaSessionFactory().getSession
+    session.init(null)
+    setup = setup + 1
+    println("initing for the %d time".format(setup))
+  }
+
+  def exit(): Unit = session.exit()
+
+  def submitLSFJob(num: Int): Unit = {
+    var jobId: String = ""
+    var outFile: String = "%s/%s-%d.txt".format(sys.env("PWD"), "scala-lsf-test", num)
+    session.synchronized {
+      if (setup < 2) {
+        session.init(null)
+        setup = setup + 1
+        println("initing for the %d time".format(setup))
+      }
+      val drmaaJob: JobTemplate = session.createJobTemplate
+      drmaaJob.setJobName("foo")
+      drmaaJob.setWorkingDirectory(sys.env("PWD"))
+      drmaaJob.setOutputPath(":" + outFile)
+      drmaaJob.setJoinFiles(true)
+      drmaaJob.setNativeSpecification("-q short")
+      drmaaJob.setRemoteCommand("/bin/echo")
+      var cmdArgs: List[String] = List("Hello", "World", "(Scala)", num.toString() )
+      drmaaJob.setArgs(cmdArgs)
+      try {
+        jobId = session.runJob(drmaaJob)
+      } catch {
+        case de: DrmaaException => throw new QException("Unable to submit job: " + de.getLocalizedMessage)
+      } finally {
+        session.deleteJobTemplate(drmaaJob)
+      }
+      println("Submitted job id: " + jobId)
     }
   }
 
+  def execute(i: Int) {
+    println("Entering jobExecutor run (%d)".format(i))
+    println("jobExecutor (%d): submitting lsf job".format(i))
+    submitLSFJob(i)
+  }
+
   def jobExecutor(i: Int): Unit = {
+    val self = this
     val job = new Thread(new Runnable {
-      def run() {
-        println("Entering jobExecutor run (%d)".format(i))
-        var session = new JnaSessionFactory().getSession
-        session.init(null)
-        println("jobExecutor (%d): submitting lsf job".format(i))
-        submitLSFJob(session, i)
-        session.exit()
+      def run() = {
+        self.execute(i)
       }
     })
     job.start()
   }
+}
 
-  def submitLSFJob(session: org.ggf.drmaa.Session, num: Int): Unit = {
-    var jobId: String = ""
-    var outFile: String = "%s/%s-%d.txt".format(sys.env("PWD"), "scala-lsf-test", num)
-    val drmaaJob: JobTemplate = session.createJobTemplate
-    drmaaJob.setJobName("foo")
-    drmaaJob.setWorkingDirectory(sys.env("PWD"))
-    drmaaJob.setOutputPath(":" + outFile)
-    drmaaJob.setJoinFiles(true)
-    drmaaJob.setNativeSpecification("-q short")
-    drmaaJob.setRemoteCommand("/bin/echo")
-    var cmdArgs: List[String] = List("Hello", "World", "(Scala)", num.toString() )
-    drmaaJob.setArgs(cmdArgs)
-    try {
-      jobId = session.runJob(drmaaJob)
-    } catch {
-      case de: DrmaaException => throw new QException("Unable to submit job: " + de.getLocalizedMessage)
-    } finally {
-      session.deleteJobTemplate(drmaaJob)
+object MultipleJobs {
+  def main(args: Array[String]): Unit = {
+    val mj = new MultipleJobs()
+    mj.init()
+    for (i <- 1 to 2) {
+      mj.jobExecutor(i)
     }
-    println("Submitted job id: " + jobId)
+    mj.exit()
   }
 }
